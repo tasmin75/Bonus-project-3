6170d18869c76df954220e7518e1481473672043
Tasmin <tansarijkm25@gmail.com>
2023-05-09 09:54:39 +0530

    Assignment is done

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { RecoilRoot } from 'recoil';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import User from './User';
+import User from './User';
+import Score from './Score';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RecoilRoot>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/User" element={<User />} />
+          <Route path="/Score" element={<Score />} />
+          {/* <App /> */}
+        </Routes>
+      </BrowserRouter>
+    </RecoilRoot>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
